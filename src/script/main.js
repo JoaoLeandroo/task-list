55dd2d2587050fdca41e1c94c1830fceb40d2c3a
joaoleandro <new.joaoleandro@gmail.com>
2023-07-13 09:41:34 -0300

    Correção bug, deletar tarefas.

--- src/script/main.js
+++ src/script/main.js
@@ -4,55 +4,53 @@ const toDoList = JSON.parse(localStorage.getItem("@taskListKey")) || [];
 
 
 function buttonAddTittleAndTask() {
-
     const titleList = prompt(`
-        Informe um Titulo
+        Informe um Título
         *Com até 30 Caracteres*
     `)
 
-    if(titleList === '' || titleList.length > 30) {
-        alert("Não salvo, informe o titulo corretamente.")
+    if (titleList === '' || titleList.length > 30) {
+        alert("Não salvo, informe o título corretamente.")
         return
     }
 
     const taskList = prompt(`
         Informe a tarefa
-        *Limite 250 caracteres"
+        *Limite 250 caracteres*
     `)
 
-    if(taskList === '' || taskList.length > 250) {
+    if (taskList === '' || taskList.length > 250) {
         alert("Não salvo, informe a tarefa corretamente.")
         return
     }
 
-    const newToDoList = structureList(titleList, taskList)
-    toDoList.unshift(newToDoList)
-    
-    renderList()
-    saveList()
+    const newToDoList = { title: titleList, task: taskList };
+    toDoList.unshift(newToDoList);
+
+    renderList();
+    saveList();
 }
+
 buttonAdd.addEventListener("click", buttonAddTittleAndTask)
 
-function structureList(title, task) {
+function structureList(title, task, index) {
     return `
         <li class="li-list">
             <h2 class="tittle-task">${title}</h2>
             <p>
                 ${task}
             </p>
-            <button class="delete-task" title="Deletar Tarefa"><div></div></button>
+            <button class="delete-task" data-index="${String(index)}" title="Deletar Tarefa"><div></div></button>
         </li>
     `
 }
-
-let postion
 
 function renderList() {
     ulList.innerHTML = ""
     ulList.innerHTML = `
         ${toDoList.map((li, index) => {
-            postion = index
-            return li
+            console.log(index)
+            return structureList(li.title, li.task, index);
         })}
     `
 
@@ -60,13 +58,13 @@ function renderList() {
     deleteButtons.forEach((item) => {
         item.addEventListener("click", deleteTask)
     })
-
 }
 
-function deleteTask() {
+function deleteTask(event) {
     const confirmDelete = prompt('Para remover uma tarefa da lista, digite "DELETAR"')
-    if(confirmDelete.toUpperCase() === 'DELETAR'){
-        toDoList.splice(postion, 1)
+    if (confirmDelete.toUpperCase() === 'DELETAR') {
+        const index = event.target.getAttribute("data-index")
+        toDoList.splice(index, 1)
         renderList()
         saveList()
         return
